83a49ee9eff7d4fb9ae7a07a8e96ed6975463ba4
MSAugdal <mathias.s.augdal@gmail.com>
2024-08-07 13:59:28 +0200

    commiting changes

--- tmux_rust_sesh.sh
+++ tmux_rust_sesh.sh
@@ -10,12 +10,12 @@ if [ $? != 0 ]
  then
     # goes to rustlings dir and open nvim in exercises dir
     tmux new-session -s $SESSIONNAME -n script -d
-    tmux send-keys -t $SESSIONNAME "cd ~/Documents/Fritidsprogrammering/rustlings" C-m
+    tmux send-keys -t $SESSIONNAME "cd ~/Documents/Programmering/rustlings" C-m
     tmux send-keys -t $SESSIONNAME "nvim ./exercises" C-m
 
     # splits horizontally, goes to rustlings dir, resizes pane, executes "rustlings watch"
     tmux split-window -h -t $SESSIONNAME
-    tmux send-keys -t $SESSIONNAME "cd ~/Documents/Fritidsprogrammering/rustlings" C-m
+    tmux send-keys -t $SESSIONNAME "cd ~/Documents/Programmering/rustlings" C-m
     tmux send-keys -t $SESSIONNAME "tmux resize-pane -R 20" C-m
     tmux send-keys -t $SESSIONNAME "rustlings watch" C-m
 
